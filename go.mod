0bdc3a06b25cad43e8f27e32f9d78b96915c58f1
tok <zoey@paradoxgarden.net>
2026-01-16 01:42:38 -0500

    working discord oauth flow

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module phragmosis
 
 go 1.25.5
 
-require golang.org/x/oauth2 v0.34.0
+require (
+	github.com/gorilla/securecookie v1.1.2
+	golang.org/x/oauth2 v0.34.0
+)
--- server.go
+++ server.go
@@ -8,8 +8,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
+	"github.com/gorilla/securecookie"
 	"golang.org/x/oauth2"
 )
 
@@ -23,23 +26,44 @@ func loadFile(path string) []byte {
 
 var atproto_redirect_string = "https://?"
 
+func getRandBytes(n int) []byte {
+	dat := make([]byte, n)
+	_, err := rand.Read(dat)
+	if err != nil {
+		panic(1)
+	}
+	return dat
+}
+
+var discord_endpoints = oauth2.Endpoint{
+	AuthURL:  "https://discord.com/oauth2/authorize",
+	TokenURL: "https://discord.com/api/oauth2/token",
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	conf := &oauth2.Config{
 		ClientID:     cfg["discord_client_id"].(string),
 		ClientSecret: cfg["discord_client_secret"].(string),
 		Scopes:       []string{"identify", "guilds"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://discord.com/oauth2/authorize",
-			TokenURL: "https://discord.com/api/oauth2/token",
-		},
+		Endpoint:     discord_endpoints,
 	}
+	dat := getRandBytes(16)
 	verf := oauth2.GenerateVerifier()
-	dat := make([]byte, 16)
-	_, err := rand.Read(dat)
-	if err != nil {
-		panic(1)
-	}
 	state := base64.URLEncoding.EncodeToString(dat)
+	enc, _ := sc.Encode("oauth_meta",map[string]string{
+		"state": state,
+		"verf": verf,
+	})
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "oauth_meta",
+		Value:    enc,
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+		MaxAge:  300,
+	})
 	url := conf.AuthCodeURL(state, oauth2.S256ChallengeOption(verf))
 	tmpl := template.Must(template.ParseFiles("./static/login.html"))
 	tmpl.Execute(w, map[string]string{
@@ -48,22 +72,54 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	})
 }
 func authHandler(w http.ResponseWriter, r *http.Request) {
+	host := r.Header.Get("X-Forwarded-Host")
+	uri := r.Header.Get("X-Forwarded-Uri")
+	original_dest := "https://" + host + uri
 	if r.Header.Get("Authorization") == "" {
-		host := r.Header.Get("X-Forwarded-Host")
-		uri := r.Header.Get("X-Forwarded-Uri")
-		original_dest := "https://" + host + uri
 		http.Redirect(w, r, fmt.Sprintf("https://%s/login?redirect=%s", cfg["hostname"], original_dest), http.StatusFound)
-
+	} else {
+		http.Redirect(w, r, original_dest, http.StatusOK)
 	}
 }
 
+var sc = securecookie.New(
+	getRandBytes(32),
+	getRandBytes(32),
+)
+
+func errChk(err error){
+	if err != nil {
+		log.Println(err)
+	}
+}
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	from := r.PathValue("provider")
 	if from == "discord" {
+		cookie, err := r.Cookie("oauth_meta")
+			errChk(err)
+		var data map[string]string
+			sc.Decode("oauth_meta",cookie.Value, &data)
+		if data["state"] != r.URL.Query().Get("state") {
+			return
+		}
+			payload := url.Values{}
+			payload.Set("grant_type", "authorization_code")
+			payload.Set("code", r.URL.Query()["code"][0])
+			payload.Set("code_verifier", data["verf"])
+			client := &http.Client{}
 
-		//oauth !
-		return
-	} else { // this is probably atproto but i'll get that exact url later
+			req, err := http.NewRequest("POST", discord_endpoints.TokenURL, strings.NewReader(payload.Encode()))
+			errChk(err)
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			req.SetBasicAuth(cfg["discord_client_id"].(string), cfg["discord_client_secret"].(string))
+		resp, err := client.Do(req)
+		log.Println(resp)
+		log.Println(err)
+		defer resp.Body.Close()
+		var res map[string]interface{}
+
+		json.NewDecoder(resp.Body).Decode(&res)
+		fmt.Println(res)
 		return
 	}
 }
